00688380e870cb6d389597e38b47e84b4d8db3b5
William Stevens <contact@wastevensv.com>
2016-05-12 18:57:50 -0400

    New project to convert obj files to something easier to work with

---
+++ sobj/obj_to_sobj.cpp
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include "verts.hpp"
+
+using namespace std;
+
+void convert(string path, vector<Vert<float, 6>> & out_vert) {
+  cout << "Loading OBJ file " << path << "..." << endl;
+  
+  ifstream in_file;
+  in_file.open (path);
+
+  if (in_file.is_open())
+  {
+    vector<Vert<float, 6>> tmp_vert;
+    vector<Vert<int, 3>> tmp_index;
+    string line;
+
+    while(!in_file.eof()) {
+      getline(in_file, line);
+
+      istringstream line_s (line);
+      string header;
+      line_s >> header;
+
+      if(header.compare("#") != 0) {
+        if(header.compare("v") == 0) {
+          Vert<float, 6> data;
+          sscanf(line.c_str(), "v %f %f %f", &data[0], &data[1], &data[2]);
+          cout << line << "|" << data << endl;
+
+          tmp_vert.push_back(data);
+
+        } else if(header.compare("f") == 0) {
+          Vert<int, 9> data;
+
+          char skip = '\0';
+
+          sscanf(line.c_str(), "f %d/%d/%d %d/%d/%d %d/%d/%d\n", &data[0], &data[1],
+            &data[2],&data[3],&data[4],&data[5],&data[6],&data[7],&data[8]);
+          cout << line << "|" << data << endl;
+
+          Vert<int, 3> index;
+          index[0] = data[0] - 1;
+          index[1] = data[3] - 1;
+          index[2] = data[6] - 1;
+          tmp_index.push_back(index);
+        }
+      }
+    }
+
+    for(int i = 0; i < tmp_index.size(); i++) {
+      out_vert.push_back(tmp_vert[tmp_index[i][0]]);
+      out_vert.push_back(tmp_vert[tmp_index[i][1]]);
+      out_vert.push_back(tmp_vert[tmp_index[i][2]]);
+    }
+    in_file.close();
+  }
+  else cerr << "Error opening the input or output file" << endl;
+}
+
+int main( int argc, char *argv[] ) {
+  vector<Vert<float, 6>> verts;
+  if(argc == 1) return -1;
+  string path = argv[1];
+  convert( path, verts );
+
+  ofstream out_file;
+  path += ".sobj";
+  out_file.open (path);
+
+  out_file << "! " << verts.size() << ' ' << '6' << endl;
+  for(int i = 0; i < verts.size(); ++i) {
+    out_file << verts[i] << endl;
+    cout << verts[i] << endl;
+  }
+  out_file.close();
+}
